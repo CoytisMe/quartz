4a339055d3c9fdee4dacacd05cb6695a5473152b
CoytisMe <coytis@coytis.me>
2026-04-15 00:20:55 +1000

    initial content

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "words.coytis.me",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
