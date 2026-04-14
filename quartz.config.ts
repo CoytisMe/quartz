6689d599002a7ce90ab6decc36af0552d79d7c14
CoytisMe <coytis@coytis.me>
2026-04-15 09:06:50 +1000

    update baseUrl to howto.coytis.me

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "words.coytis.me",
+    baseUrl: "howto.coytis.me",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
